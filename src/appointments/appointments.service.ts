e81b282c35debd71e4489bad55796bdff6f42d1a
MateusLeonardo <mateus_leonardo1997@hotmail.com>
2025-04-23 13:27:31 -0300

    feat: enhance appointment creation to check for conflicting appointments for patients and doctors

--- src/appointments/appointments.service.ts
+++ src/appointments/appointments.service.ts
@@ -1,18 +1,114 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
 import { CreateAppointmentDto } from './dto/create-appointment.dto';
 import { UpdateAppointmentDto } from './dto/update-appointment.dto';
 import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { DoctorsService } from 'src/doctors/doctors.service';
+import { PatientsService } from 'src/patients/patients.service';
 
 @Injectable()
 export class AppointmentsService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly doctorsService: DoctorsService,
+    private readonly patientsService: PatientsService,
+  ) {}
 
   async create({ id }: User, createAppointmentDto: CreateAppointmentDto) {
-    // todo: verificar se o paciente existe
-    // todo: verificar se o doutor existe
-    // todo: verificar se o paciente já tem consulta marcada para o mesmo dia e horário
-    // todo: verificar se o doutor já tem consulta marcada para o mesmo dia e horário
+    const { doctorId, patientId, date, duration } = createAppointmentDto;
+    const appointmentDate = new Date(date);
+    const appointmentEndTime = new Date(appointmentDate);
+    appointmentEndTime.setMinutes(
+      appointmentEndTime.getMinutes() + (duration || 30),
+    );
+
+    if (appointmentDate < new Date()) {
+      throw new BadRequestException(
+        'Não é possível agendar para datas passadas',
+      );
+    }
+
+    return this.prismaService.$transaction(async (prisma) => {
+      const doctor = await this.doctorsService.findOne(doctorId);
+      const patient = await this.patientsService.findOne(patientId);
+
+      const attendant = await prisma.attendant.findUnique({
+        where: { userId: id },
+      });
+
+      const conflictingPatientAppointments = await prisma.appointment.findMany({
+        where: {
+          patientId: patient.userId,
+          date: { lt: appointmentEndTime },
+          OR: [
+            {
+              date: { gte: appointmentDate },
+            },
+            {
+              date: {
+                lt: appointmentDate,
+              },
+              duration: {
+                gt: this.minutesBetween(appointmentDate, new Date(date)),
+              },
+            },
+          ],
+        },
+      });
+
+      const conflictingDoctorAppointments = await prisma.appointment.findMany({
+        where: {
+          doctorId: doctor.userId,
+          date: { lt: appointmentEndTime },
+          OR: [
+            {
+              date: { gte: appointmentDate },
+            },
+            {
+              date: {
+                lt: appointmentDate,
+              },
+              duration: {
+                gt: this.minutesBetween(appointmentDate, new Date(date)),
+              },
+            },
+          ],
+        },
+      });
+
+      if (conflictingPatientAppointments.length > 0) {
+        throw new ConflictException(
+          'Paciente já possui consulta marcada para este horário',
+        );
+      }
+
+      if (conflictingDoctorAppointments.length > 0) {
+        throw new ConflictException(
+          'Doutor já possui consulta marcada para este horário',
+        );
+      }
+
+      return prisma.appointment.create({
+        data: {
+          date: appointmentDate,
+          status: 'PENDING',
+          reason: createAppointmentDto.reason,
+          notes: createAppointmentDto.notes,
+          duration: duration || 30,
+          patientId: patient.userId,
+          doctorId: doctor.userId,
+          attendantId: attendant?.id,
+        },
+      });
+    });
+  }
+
+  private minutesBetween(date1: Date, date2: Date): number {
+    return Math.floor((date2.getTime() - date1.getTime()) / (1000 * 60));
   }
 
   findAll() {
