514fcfeade5dcd11bb0c011201147a85fe418fc0
MateusLeonardo <mateus_leonardo1997@hotmail.com>
2025-04-28 11:16:26 -0300

    feat: implement CreateMedicalRecordDto with validation rules

--- src/medical-records/dto/create-medical-record.dto.ts
+++ src/medical-records/dto/create-medical-record.dto.ts
@@ -1,1 +1,20 @@
-export class CreateMedicalRecordDto {}
+import { IsNumber, IsOptional, IsString } from "class-validator";
+
+export class CreateMedicalRecordDto {
+    @IsNumber()
+    patientId: number;
+
+    @IsNumber()
+    doctorId: number;
+
+    @IsString()
+    diagnosis: string;
+
+    @IsString()
+    @IsOptional()
+    prescription?: string;
+
+    @IsString()
+    @IsOptional()
+    notes?: string;
+}
